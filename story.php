ec1bfc5392eaeee7f46875ac1dfd5e86fcc521f5
lambertclement <lambertclement26@gmail.com>
2022-03-06 19:36:35 +0100

    Début html story

--- story.php
+++ story.php
@@ -21,8 +21,52 @@
 <img src="cdginauguration.jpg" alt="story"> 
 </div>
 
+<h2> L'aéroport de Paris-Orly fête en 2021 ses 60 ans </h2> 
+
+<p> Le 24 février 1961, le président Charles de Gaulle inaugure en grande pompe l'aérogare d'Orly Sud. 
+Première grande réalisation de l'Aéroport de Paris, qui va y installer les métiers phares de l'entreprise 
+- conception et exploitation aéroportuaires - le nouvel aéroport fait entrer définitivement Paris dans la modernité et positionne à nouveau la France comme une grande puissance mondiale. 
+Retour sur 60 ans d'Histoire ! </p> 
+
+<p> A peine 16 ans après la Seconde Guerre mondiale, Orly est le symbole tangible d'un monde en plein changement. En effet, comme toutes les grandes infrastructures de transport, le nouvel grand aéroport de l'Ouest de Paris s'adapte aux flux et aux nouvelles technologies. Véritable prouesse architecturale, technique et esthétique, il représente non seulement le présent, mais aussi le futur d'une France en mouvement. </p>
+
+<h3> Une modernité et une technique selon les architectes Henri Vicariot et Jean Prouvé </h3>
+
+<div class ="photo"> 
+<img src="orly1960.jpg" alt="story"> 
+</div>
+
+<div class ="photo"> 
+<img src="orly1960.jpg" alt="story"> 
+</div>
 
 
+
+
+
+
+
+²	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- ATTENTION import Pied de page -->
+    <?php include 'footer.php' ?>
+
+</body>
+</html>
 
 
 
