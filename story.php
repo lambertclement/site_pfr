f87056136cb669af1054d61a4dc2a3f783fb40c6
lambertclement <lambertclement26@gmail.com>
2022-03-06 19:21:52 +0100

    Création fichier

--- story.php
+++ story.php
@@ -7,14 +7,51 @@
 
 </head>
 <body>
+
+<meta charset="utf-8">
+<link rel="stylesheet" href="quisommesnous.css"/> 
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,300&display=swap" rel="stylesheet"></head>
+
     <!-- ATTENTION barre de navigation -->
     <?php include 'menu.php' ?>
 
     <h1>Histoire de l'aéroport</h1>
 
+<div class ="photo"> 
+<img src="cdginauguration.jpg" alt="story"> 
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <!-- ATTENTION import Pied de page -->
     <?php include 'footer.php' ?>
-
 
 </body>
 </html>
