3d86ffc3375490a637eea436b1403304a5bd28bf
lambertclement <lambertclement26@gmail.com>
2022-03-06 20:14:10 +0100

    Version semi-final story

--- story.php
+++ story.php
@@ -37,61 +37,44 @@ Retour sur 60 ans d'Histoire ! </p>
 </div>
 
 <div class ="photo"> 
-<img src="orly1960.jpg" alt="story"> 
+<img src="orlyinau.jpg" alt="story"> 
 </div>
 
+<p> Henri Vicariot est le principal architecte de l’aérogare d'Orly Sud. Il s'associe notamment à Jean Prouvé à qui il laisse l'aménagement intérieur du bâtiment.
 
+Charpente d'acier et de béton, murs rideaux (en verre) développés par Saint-Gobain, sonorisation de l'aéroport inédite avec 3000 haut-parleurs, lumière omniprésente dans toute l'aérogare, terrasse, etc. 
 
+Orly Sud devient à partir de 1961 une véritable attraction touristique où l'on vient en famille le dimanche se balader et regarder les avions décoller. </p> 
 
+<p> Si jamais un ouvrage justifia la fierté de ceux qui l’ont édifié de leurs cerveaux et de leurs mains, c’est bien celui que voilà, à la rencontre du ciel et de la terre.
+Le président Charles de Gaulle
+Extrait de son discours lors de l'inauguration d'Orly Sud en 1961 </p> 
 
+<h4> Une esthétique de luxe qui allie bien-être et plaisir </h4>
 
+<div class ="photo"> 
+<img src="luxe.jpg" alt="story"> 
+</div>
 
-²	
+<div class ="photo"> 
+<img src="bar.jpg" alt="story"> 
+</div>
 
+<p> Du choix du mobilier, à la galerie marchande et la zone de transit, en passant par les restaurants, le cinéma et l'hôtel, ainsi que les tenues de grands couturiers du personnel d'accueil, rien n'est oublié pour faire vivre aux passagers un moment unique et privilégié. Une manifestation du chic parisien et du luxe international.
 
+Enfin, l'aéroport d'Orly est tellement populaire qu'il devient dans les années 1960 le décor de nombreux films et le passage obligé de nombreuses personnalités. </p> 
 
+<video width=600  height=480 controls poster="vignette.jpg">
+  <source src="Orly, aéroport des sixties, entre histoire et photographie.mp4" type="video/mp4">
+</video>
 
+<p> Interview de Paul Damm, conservateur du patrimoine et auteur (avec Laurence Bartoletti) de Orly, Aéroport des Sixties, livre paru en 2020 aux éditions Lieux-Dits, en partenariat avec le Groupe ADP et le musée Air France. </p>
 
+<p> Avec la construction de l'aéroport Paris-Orly, c'est enfin tout le réseau urbain du sud parisien qui se dessine autour et vers l'aéroport.
 
+De nos jours, l'aéroport de Paris-Orly possède quatre sections : Orly 1, Orly 2, Orly 3 et Orly 4. C'est le deuxième aéroport parisien après Paris-Charles de Gaulle. Il est relié directement par le RER B et l'OrlyVal.
 
-
-
-
-
-
-
-
-
-    <!-- ATTENTION import Pied de page -->
-    <?php include 'footer.php' ?>
-
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+En 2019, près de 32 000 de passagers sont passés par cet aéroport mythique. </p>
 
 
     <!-- ATTENTION import Pied de page -->
